c141e54dfa616d1a197ddb47b42c8ab601a23669
Egor <sparkjetstudiost@gmail.com>
2020-04-03 22:51:51 +0300

    add new view class that show edit form

--- mWallet/profiles/views.py
+++ mWallet/profiles/views.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic.base import TemplateView
-from django.shortcuts import render
+from django.views.generic.edit import FormView
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from accounts.models import Person
+from .tools import create_download_files
+from .forms import PersonEditForm
 
 
 class ProfileView(TemplateView):
@@ -11,6 +17,7 @@ class ProfileView(TemplateView):
     # login_required can't work without it
     @method_decorator(login_required)
     def get(self, request, *args, **kwargs):
+        create_download_files(pk=self.request.user.pk)
         return render(request, self.template_name, self.get_context_data())
 
     def get_context_data(self, **kwargs):
@@ -19,6 +26,44 @@ class ProfileView(TemplateView):
             'title': 'Profile',
             'profile_a': 'active',
             'profile_d': 'disabled',
+        })
+
+        return ctx
+
+
+class EditProfile(FormView):
+    form_class = PersonEditForm
+    template_name = 'profiles/edit.html'
+
+    @method_decorator(login_required)
+    def get(self, request, *args, **kwargs):
+        form = PersonEditForm(instance=request.user)
+
+        ctx = self.get_context_data()
+        ctx['form'] = form
+
+        return render(request, self.template_name, ctx)
+
+    @method_decorator(login_required)
+    def post(self, request, *args, **kwargs):
+        form = PersonEditForm(self.request.POST, instance=self.request.user)
+
+        ctx = self.get_context_data()
+        ctx['form'] = form
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, 'Profile successfully changed.')
+            return redirect('profiles:edit_profile')
+
+        return render(request, self.template_name, ctx)
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx.update({
+            'title': 'Edit Profile',
+            'settings_a': 'active',
+            'settings_d': 'disabled',
         })
 
         return ctx

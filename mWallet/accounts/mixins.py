c8ea12fdc6413eb437b4b8bfc4b2fb21d0e23250
Egor <sparkjetstudiost@gmail.com>
2020-04-03 22:40:56 +0300

    added min length to middle name

--- mWallet/accounts/mixins.py
+++ mWallet/accounts/mixins.py
@@ -1,11 +1,17 @@
 # https://stackoverflow.com/questions/19130942/whats-the-best-way-to-store-phone-number-in-django-models
 from phonenumber_field.modelfields import PhoneNumberField
 from django.utils.translation import gettext_lazy as _
+from django.core.validators import MinLengthValidator
 from django.db import models
 
 
 class PersonMixin(models.Model):
-    middle_name = models.CharField(_('Middle name'), max_length=70, blank=True)
+    middle_name = models.CharField(
+        _('Middle name'),
+        max_length=70,
+        blank=True,
+        validators=[MinLengthValidator(2)]
+    )
     birth_date = models.DateField(
         _('Your birth date'),
         blank=True,

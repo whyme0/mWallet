1492549b52b7f988a462d26e2372f98ea8a92a69
Egor <sparkjetstudiost@gmail.com>
2020-04-04 19:48:26 +0300

    added two new urls to deleting account

--- mWallet/profiles/urls.py
+++ mWallet/profiles/urls.py
@@ -6,4 +6,6 @@ app_name = 'profiles'
 urlpatterns = [
     path('', views.ProfileView.as_view(), name='current_profile'),
     path('edit', views.EditProfile.as_view(), name='edit_profile'),
+    path('edit/deleting', views.DeleteConfirmation.as_view(), name='profile_deleting'),
+    path('edit/deleting/delete', views.Delete.as_view(), name='profile_delete'),
 ]

b4135bd12df71a354242ac483d1a7ec0d777296b
Egor <sparkjetstudiost@gmail.com>
2020-04-05 16:03:45 +0300

    added url path for wallets list

--- mWallet/profiles/urls.py
+++ mWallet/profiles/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('edit', views.EditProfile.as_view(), name='edit_profile'),
     path('edit/deleting', views.DeleteConfirmation.as_view(), name='profile_deleting'),
     path('edit/deleting/delete', views.Delete.as_view(), name='profile_delete'),
+    path('wallets', views.WalletsView.as_view(), name='profile_wallets'),
+    # path('wallets/create', views.WalletsCreation.as_view(), name='wallet_creation'),
 ]

11b9e8f4207a84dfb58d474715d47ba392c42970
Egor <sparkjetstudiost@gmail.com>
2020-04-06 21:27:04 +0300

    added url path to wallet creation

--- mWallet/profiles/urls.py
+++ mWallet/profiles/urls.py
@@ -5,9 +5,10 @@ from profiles import views
 app_name = 'profiles'
 urlpatterns = [
     path('', views.ProfileView.as_view(), name='current_profile'),
-    path('edit', views.EditProfile.as_view(), name='edit_profile'),
+    path('edit/', views.EditProfile.as_view(), name='edit_profile'),
     path('edit/deleting', views.DeleteConfirmation.as_view(), name='profile_deleting'),
     path('edit/deleting/delete', views.Delete.as_view(), name='profile_delete'),
-    path('wallets', views.WalletsView.as_view(), name='profile_wallets'),
-    # path('wallets/create', views.WalletsCreation.as_view(), name='wallet_creation'),
+    path('wallets/', views.WalletsView.as_view(), name='profile_wallets'),
+    path('wallets/create', views.WalletCreationView.as_view(), name='wallet_creation'),
+    # path('wallets/<pk:int>/edit', views.WalletsEditView.as_view(), name='current_wallet'),
 ]

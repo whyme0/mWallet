b36760a5e2cefbcb39a42a71a0d17cb751347616
Egor <sparkjetstudiost@gmail.com>
2020-04-07 22:23:59 +0300

    added url paths for edit and deletion wallet

--- mWallet/profiles/urls.py
+++ mWallet/profiles/urls.py
@@ -6,9 +6,11 @@ app_name = 'profiles'
 urlpatterns = [
     path('', views.ProfileView.as_view(), name='current_profile'),
     path('edit/', views.EditProfile.as_view(), name='edit_profile'),
-    path('edit/deleting', views.DeleteConfirmation.as_view(), name='profile_deleting'),
-    path('edit/deleting/delete', views.Delete.as_view(), name='profile_delete'),
+    path('edit/deleting', views.PersonDeleteConfirmation.as_view(), name='profile_deleting'),
+    path('edit/deleting/delete', views.PersonDelete.as_view(), name='profile_delete'),
     path('wallets/', views.WalletsView.as_view(), name='profile_wallets'),
     path('wallets/create', views.WalletCreationView.as_view(), name='wallet_creation'),
-    # path('wallets/<pk:int>/edit', views.WalletsEditView.as_view(), name='current_wallet'),
+    path('wallets/<int:pk>', views.WalletInfoView.as_view(), name='current_wallet'),
+    path('wallets/<int:pk>/edit', views.WalletEditView.as_view(), name='wallet_edit'),
+    path('wallets/<int:pk>/delete', views.WalletDelete.as_view(), name='wallet_delete'),
 ]

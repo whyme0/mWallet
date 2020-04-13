8fee2b5e36f3eb96331b9feca883bc4e0246117c
Egor <sparkjetstudiost@gmail.com>
2020-04-13 23:22:25 +0300

    added url path to password change view

--- mWallet/authapp/urls.py
+++ mWallet/authapp/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'authapp'
 urlpatterns = [
     path('logout/', views.log_out, name='logout'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('password-change/', views.PasswordChangeView.as_view(), name='password_change'),
 ]

38eb4a2f80f770789b87ed671bc64bdc62c5dc1e
Egor <sparkjetstudiost@gmail.com>
2020-04-14 22:28:50 +0300

    commit

--- mWallet/authapp/forms.py
+++ mWallet/authapp/forms.py
@@ -1,4 +1,8 @@
-from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.contrib.auth.forms import (
+    AuthenticationForm, PasswordChangeForm,
+    UserCreationForm,)
+
+from accounts.models import Person
 
 
 class LoginForm(AuthenticationForm):
@@ -7,10 +11,27 @@ class LoginForm(AuthenticationForm):
 
         self.fields['username'].label = 'Email'
 
-class PasswordChange(PasswordChangeForm):
-	def __init__(self, *args, **kwargs):
-		super(PasswordChange, self).__init__(*args, **kwargs)
 
-		self.fields['old_password'].widget.attrs['class'] = 'old_password'
-		self.fields['new_password1'].help_text = None
-		self.fields['new_password2'].label = 'Confirm new password'
+class PasswordChange(PasswordChangeForm):
+    def __init__(self, *args, **kwargs):
+        super(PasswordChange, self).__init__(*args, **kwargs)
+
+        self.fields['old_password'].widget.attrs['class'] = 'old_password'
+        self.fields['new_password1'].help_text = None
+        self.fields['new_password2'].label = 'Confirm new password'
+
+
+class RegistrationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+
+        self.fields['password1'].help_text = None
+
+    class Meta:
+        model = Person
+        fields = [
+            'first_name', 'middle_name',
+            'last_name', 'email', 'phone_number',
+            'living_place', 'birth_date',
+            'password1', 'password2',
+        ]

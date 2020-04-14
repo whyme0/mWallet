862074ab890fed9efe6168c9b5c6c25c43039a34
Egor <sparkjetstudiost@gmail.com>
2020-04-14 22:28:07 +0300

    commit

--- mWallet/authapp/views.py
+++ mWallet/authapp/views.py
@@ -1,12 +1,12 @@
+from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.views import PasswordChangeView
+from django.views.decorators.cache import never_cache
 from django.views.generic.edit import FormView
-from django.contrib.auth import logout, login
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.contrib import messages
 
-from .authentication import PhoneOrEmailBackend
-from .forms import LoginForm, PasswordChange
+from . import forms
 
 
 def log_out(request):
@@ -16,19 +16,34 @@ def log_out(request):
     return redirect('home')
 
 
+class RegistrationView(FormView):
+    form_class = forms.RegistrationForm
+    template_name = 'authapp/registration.html'
+
+    def form_valid(self, form):
+        # after succefull registration person
+        # sign in system and redirect to
+        # profile page
+        return None
+
+
 class LoginView(FormView):
     template_name = 'authapp/login.html'
-    form_class = LoginForm
+    form_class = forms.LoginForm
     extra_context = {'title': 'Login'}
 
     def form_valid(self, form):
-        person = PhoneOrEmailBackend().authenticate(
+        person = authenticate(
             self.request,
             email=form.cleaned_data['username'],
             password=form.cleaned_data['password']
         )
 
-        login(self.request, person, backend='authapp.authentication.PhoneOrEmailBackend')
+        login(
+            self.request,
+            person,
+            backend='authapp.authentication.PhoneOrEmailBackend'
+        )
         messages.success(self.request, 'You successfully logged in.')
 
         return redirect(self.get_success_url())
@@ -39,11 +54,19 @@ class LoginView(FormView):
         else:
             return reverse_lazy('profiles:current_profile')
 
+    def get(self, request, *args, **kwargs):
+        if self.request.GET.get('next'):
+            message = 'You must sign in before see this page'
+            self.extra_context['error_message'] = message
+        else:
+            self.extra_context['error_message'] = ''
+        return super().get(request, *args, **kwargs)
+
 
 class PasswordChangeView(PasswordChangeView):
     template_name = 'authapp/password-change.html'
     extra_context = {'title': 'Password change'}
-    form_class = PasswordChange
+    form_class = forms.PasswordChange
 
     def form_valid(self, form):
         messages.success(self.request, 'Password changed successfully.')

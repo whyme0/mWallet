044924b25b57e8bfc1c3e8fc114377ae9c345d08
Egor <sparkjetstudiost@gmail.com>
2020-04-18 22:20:00 +0300

    commit

--- mWallet/base/forms.py
+++ mWallet/base/forms.py
@@ -1,16 +1,19 @@
-from django.contrib import forms
+from django import forms
 
 from . import validators
 
 
 class FeedbackForm(forms.Form):
+    feedback_title = forms.CharField(label='Title')
+
+    feedback_message = forms.CharField(
+        label='Message',
+        validators=[validators.check_message],
+        widget=forms.Textarea,
+        help_text='Leave your feedback in this field',
+    )
+
     email = forms.EmailField(
         validators=[validators.email_exist],
         help_text='This email must be already registred.',
-    )
-
-    feeback_message = forms.CharField(
-        validators=[validators.check_empty],
-        widget=forms.Textarea,
-        help_text='Leave your feedback in this field',
     )

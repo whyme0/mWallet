46b93d5558dc956626bc4510a7b55819c26811f2
Egor <sparkjetstudiost@gmail.com>
2020-04-18 22:16:49 +0300

    updated form_valid method in FeedbackView

--- mWallet/base/views.py
+++ mWallet/base/views.py
@@ -1,11 +1,14 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
-from django.views.forms import FormView
-from django.core.mail import send_email
+from django.views.generic.edit import FormView
+from django.core.mail import send_mail
 from django.urls import reverse_lazy
+from django.contrib import messages
 
 from . import tools
 from . import forms
+
+from settings import settings
 
 class HomeView(TemplateView):
     template_name = 'base/home.html'
@@ -22,15 +25,21 @@ class HomeView(TemplateView):
 
 class FeedbackView(FormView):
     form_class = forms.FeedbackForm
-    template_name = ''
+    template_name = 'base/feedback.html'
     extra_context = {
         'title': 'Feedback',
         'token': tools.get_random_key(),
     }
 
     def form_valid(self, form):
-        send_email()
-        messages.success('Thanks for your feedback. We will certainly consider it.')
+        send_mail(
+            form.cleaned_data['feedback_title'].strip(),
+            form.cleaned_data['feedback_message'].strip(),
+            settings.EMAIL_HOST_USER,
+            ['real-temp@hubopss.com'],
+            fail_silently=False,
+        )
+        messages.success(self.request, 'Thanks for your feedback. We will certainly consider it.')
         return redirect(reverse_lazy('feedback'))
 
 

2488741d1bde4e13374f2adec7cf0f3af857546a
Egor <sparkjetstudiost@gmail.com>
2020-04-18 15:04:06 +0300

    created class with feedback view

--- mWallet/base/views.py
+++ mWallet/base/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.views.forms import FormView
+from django.core.mail import send_email
+from django.urls import reverse_lazy
 
+from . import tools
+from . import forms
 
 class HomeView(TemplateView):
     template_name = 'base/home.html'
@@ -13,6 +18,20 @@ class HomeView(TemplateView):
             self.template_name,
             super().get_context_data(**kwargs),
         )
+
+
+class FeedbackView(FormView):
+    form_class = forms.FeedbackForm
+    template_name = ''
+    extra_context = {
+        'title': 'Feedback',
+        'token': tools.get_random_key(),
+    }
+
+    def form_valid(self, form):
+        send_email()
+        messages.success('Thanks for your feedback. We will certainly consider it.')
+        return redirect(reverse_lazy('feedback'))
 
 
 def error_404(request, exception):

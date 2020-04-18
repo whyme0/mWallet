d382bcadb3b7460a1f4f8d427f43f62b9ff87655
Egor <sparkjetstudiost@gmail.com>
2020-04-18 14:45:46 +0300

    created file with validators for feedback form

---
+++ mWallet/base/validators.py
@@ -0,0 +1,23 @@
+from django.core.exceptions import ValidationError
+from django.utils.translations import gettext_lazy as _
+
+from accounts.models import Person
+
+
+def email_exist(value):
+    try:
+        # check if email exist
+        Person.objects.get(email=value)
+    except Person.DoesNotExist:
+        raise ValidationError(
+            _('User with this email doesn\'t exist.'),
+            params={'value': value},
+        )
+
+
+def check_empty(value):
+    if len(value.strip()) < 20:
+        raise ValidationError(
+            _('Your message seems really small, '
+              'try to describe problem with more details')
+        )

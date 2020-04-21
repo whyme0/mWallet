8a746717eeec8a62f6e1b101813f7306b563edaa
Egor <sparkjetstudiost@gmail.com>
2020-04-21 21:17:38 +0300

    folder with password, secret tokens, etc.

---
+++ mWallet/unpublic/security.py
@@ -0,0 +1,9 @@
+# ----------------------------------------------- #
+#  Use your personal data for right project work  #
+# ----------------------------------------------- #
+
+YA_EMAIL = ''
+YA_PASSWORD = ''
+
+GOOGLE_RECAPTCHA_SECRET_KEY = ''
+GOOGLE_RECAPTCHA_PUBLIC_KEY = ''

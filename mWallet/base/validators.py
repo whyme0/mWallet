d37b12d8cda365fbf3c541fe3f8db303e6811bde
Egor <sparkjetstudiost@gmail.com>
2020-04-25 11:45:27 +0300

    added new validator

--- mWallet/base/validators.py
+++ mWallet/base/validators.py
@@ -2,9 +2,15 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
 from accounts.models import Person
+from authapp.models import Token
+from base import tools
 
 
-def email_exist(value):
+def only_email_exist(value):
+    '''
+    Validator allows you to submit the
+    form only if the entered email exists
+    '''
     try:
         # check if email exist
         Person.objects.get(email=value)
@@ -21,3 +27,20 @@ def check_message(value):
             _('Your message seems really small, '
               'try to describe problem with more details')
         )
+
+
+def ban_existing_email(value):
+    '''
+    Forbid ask again password reset link, while token
+    for this email exist.
+    '''
+    if tools.is_email_exist(value):
+        person = Person.objects.get(email=value)
+        try:
+            Token.objects.get(person=person)
+            raise ValidationError(
+                _('Password reset link for your account was already sent. Try again later.'),
+                params={'value': value},
+            )
+        except Token.DoesNotExist:
+            pass

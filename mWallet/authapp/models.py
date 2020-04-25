ab79e26c7eaac8aca3013a8d72f0d0f0dc93fe8b
Egor <sparkjetstudiost@gmail.com>
2020-04-25 12:15:04 +0300

    created model for Tokens

--- mWallet/authapp/models.py
+++ mWallet/authapp/models.py
@@ -1,3 +1,16 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
-# Create your models here.
+from accounts.models import Person
+
+
+class Token(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    token = models.CharField(
+        max_length=20,
+        validators=[MinLengthValidator(20)],
+        help_text='Don\'t change token manually. It\'s exist to reset user password'
+    )
+
+    def __str__(self):
+        return f'Token for {self.person}'

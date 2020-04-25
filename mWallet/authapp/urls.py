abc33044f701e34f787f37df2347e2d670da6c5a
Egor <sparkjetstudiost@gmail.com>
2020-04-25 12:14:31 +0300

    added to urls path for ask email and password reset

--- mWallet/authapp/urls.py
+++ mWallet/authapp/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('logout/', views.log_out, name='logout'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('password-change/', views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-reset/', views.AskEmailView.as_view(), name='ask_email'),
+    path('password-reset/<str:token>/', views.PasswordResetView.as_view(), name='password_reset')
 ]

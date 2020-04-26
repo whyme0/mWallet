75de08ea124fcb42a35d4dd15ece7417c37382e5
Egor <sparkjetstudiost@gmail.com>
2020-04-26 17:48:09 +0300

    created url path for registration

--- mWallet/authapp/urls.py
+++ mWallet/authapp/urls.py
@@ -6,6 +6,7 @@ app_name = 'authapp'
 urlpatterns = [
     path('logout/', views.log_out, name='logout'),
     path('login/', views.LoginView.as_view(), name='login'),
+    path('registration/', views.RegistrationView.as_view(), name='registration'),
     path('password-change/', views.PasswordChangeView.as_view(), name='password_change'),
     path('password-reset/', views.AskEmailView.as_view(), name='ask_email'),
     path('password-reset/<str:token>/', views.PasswordResetView.as_view(), name='password_reset')

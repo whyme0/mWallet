088d14fbc893ff9dac1b7de9701a5b6902529d50
Egor <sparkjetstudiost@gmail.com>
2020-04-27 15:34:22 +0300

    commit

--- mWallet/settings/settings.py
+++ mWallet/settings/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
